5f79a08605ea91bc7801e11e1d168a621debac60
developeravikr <109568405+developeravikr@users.noreply.github.com>
2022-07-29 09:36:42 +0530

    Updates

---
+++ precache-manifest.cac02d45da5da5c6f0da40adec3f0b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f83af7633541936e385",
+    "url": "/Text-Utils/static/css/main.5bd94422.chunk.css"
+  },
+  {
+    "revision": "4f83af7633541936e385",
+    "url": "/Text-Utils/static/js/main.4f83af76.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Text-Utils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "1b46f961a433862e2504",
+    "url": "/Text-Utils/static/js/2.1b46f961.chunk.js"
+  },
+  {
+    "revision": "2083fbf419ecca366ee4",
+    "url": "/Text-Utils/static/js/runtime~main.2083fbf4.js"
+  },
+  {
+    "revision": "bcbd94f566c5f0907d21a9dffa40dded",
+    "url": "/Text-Utils/index.html"
+  }
+];
